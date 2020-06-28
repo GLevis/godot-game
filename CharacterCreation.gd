9ff018cf3b809b3f434c6887db3cf6d261d9a9fd
Palmerb01 <57827797+Palmerb01@users.noreply.github.com>
2020-06-28 04:50:58 -0500

    roll for stats

---
+++ CharacterCreation.gd
@@ -0,0 +1,37 @@
+extends Control
+
+func _ready():
+	$Scroll/CharFrame/Sprite/AnimationPlayer.play("idle")
+
+
+func _on_RollButton_pressed():
+	var statArray = roll_stats()
+	
+	$Scroll/StatBlock/HpAmnt.set_text(str(statArray[0]))
+	$Scroll/StatBlock/AtkAmnt.set_text(str(statArray[1]))
+	$Scroll/StatBlock/DefAmnt.set_text(str(statArray[2]))
+	$Scroll/StatBlock/IntAmnt.set_text(str(statArray[3]))
+	$Scroll/StatBlock/SpdAmnt.set_text(str(statArray[4]))
+	$Scroll/StatBlock/StamAmnt.set_text(str(statArray[5]))
+	
+	
+func roll_stats():
+	var counter = 0
+	var stats = []
+	while counter < 6:
+		randomize()
+		var num1 = randi() % 6 + 1
+		randomize()
+		var num2 = randi() % 6 + 1
+		randomize()
+		var num3 = randi() % 6 + 1
+		randomize()
+		var num4 = randi() % 6 + 1
+		var stat = [num1, num2, num3, num4]
+		stat.sort()
+		stat.remove(0)
+		var stats_added = stat[0] + stat[1] + stat[2]
+		stats.push_back(stats_added)
+		counter += 1
+	return stats
+		
