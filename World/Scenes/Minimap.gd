28d8008b52f2a88b2a54d3a3dd877c65f438f938
Palmerb01 <57827797+Palmerb01@users.noreply.github.com>
2020-07-15 17:40:34 -0500

    minimappppp
    no work rite awy

--- World/Scenes/Minimap.gd
+++ World/Scenes/Minimap.gd
@@ -6,19 +6,22 @@ var shown_tiles = []
 var current_pos = Vector2()
 var player_pos = Vector2()
 
+var tilemap
+
 func _ready():
+	tilemap = get_tree().get_root().get_node("TileMap")
 	yield(get_tree(), "idle_frame")
-	current_pos = Vector2(get_parent().player.position.x / 32, get_parent().player.position.y  / 32)
+	current_pos = Vector2(tilemap.player.position.x / 32, tilemap.player.position.y  / 32)
 	player_pos = current_pos
 	set_cellv(current_pos, 1)
-	for x in range(0, get_parent().map_w):
-		for y in range(0, get_parent().map_h):
-			if get_parent().get_cellv(Vector2(x,y)) == get_parent().Tiles.FLOOR:
+	for x in range(0, tilemap.map_w):
+		for y in range(0, tilemap.map_h):
+			if tilemap.get_cellv(Vector2(x,y)) == tilemap.Tiles.FLOOR:
 				hidden_tiles.append(Vector2(x,y))
 
 func _process(_delta):
-	position = get_parent().player.position - Vector2(210, 120)
-	player_pos = Vector2(get_parent().player.position.x / 32, get_parent().player.position.y / 32)
+	position = tilemap.player.position - Vector2(210, 120)
+	player_pos = Vector2(tilemap.player.position.x / 32, tilemap.player.position.y / 32)
 	if player_pos != current_pos:
 		set_cellv(current_pos, 1)
 		set_cellv(player_pos, 0)
