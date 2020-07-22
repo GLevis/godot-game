71cf13ff1a846e3dc63a0e895e898305bcc6180d
GLevis <levis.gavin@gmail.com>
2020-07-22 13:58:18 -0500

    ui update

--- GUI/Inventory/New Inv/DraggableBar.gd
+++ GUI/Inventory/New Inv/DraggableBar.gd
@@ -3,13 +3,23 @@ extends Panel
 var parent = null;
 var drag = false;
 var offset = Vector2(0, 0);
+var status
 
 func _ready():
 	parent = get_parent();
 
 func _input(event : InputEvent):
 	if parent && drag:
-		parent.rect_global_position = event.global_position - offset;
+		status = "clicked"
+	
+	if status == "clicked" && event is InputEventMouseMotion:
+		status = "dragging"
+	
+	if status == "dragging":
+		if event.get_button_mask() != BUTTON_LEFT:
+			status = "released"
+		else:
+			parent.rect_global_position = event.global_position
 
 func _gui_input(event : InputEvent):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
